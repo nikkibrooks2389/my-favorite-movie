85ca954e8abb5b7c0fd0cdb6bc82e5d1f61ee6e8
nikkibrooks2389 <nbrooks2389@gmail.com>
2023-09-28 13:46:37 -0700

    add files and folders for main outline and download packages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
+
+// Components
+import { MovieList, MovieDetails } from './components/Movie';
+import { FavoritesList } from './components/Favorites';
+import RandomMovieSelector from './components/RandomMovieSelector';
+
+// Styled Components
+const AppContainer = styled.div`
+  font-family: Arial, sans-serif;
+  background-color: #f4f4f4;
+  min-height: 100vh;
+  padding: 20px;
+`;
+
+const AppHeader = styled.header`
+  background-color: #282c34;
+  padding: 20px 0;
+  text-align: center;
+  color: white;
+`;
 
 function App() {
+  const favorites = useSelector(state => state.favorites);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppContainer>
+        <AppHeader>
+          <h1>My Favorite Movie</h1>
+        </AppHeader>
+
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <MovieList />
+            </Route>
+
+            <Route path="/movie/:movieId">
+              <MovieDetails />
+            </Route>
+
+            <Route path="/favorites">
+              <FavoritesList movies={favorites} />
+            </Route>
+
+            <Route path="/random">
+              <RandomMovieSelector />
+            </Route>
+
+            <Route>
+              <div>404: Page Not Found</div>
+            </Route>
+          </Switch>
+        </main>
+      </AppContainer>
+    </Router>
   );
 }
 
